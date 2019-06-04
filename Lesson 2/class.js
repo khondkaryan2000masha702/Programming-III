f8f488ddfafd5b4dc0b7f988755ffb9a4283d727
user <user@lab2108m.tumo.lab>
2019-06-04 17:56:51 +0400

    changed class.js

--- Lesson 2/class.js
+++ Lesson 2/class.js
@@ -1,3 +1,36 @@
+class LivingCreature {
+    constructor(x, y, index){
+        this.x = x;
+        this.y = y;
+        this.multiply = 0;
+        this.index = index;
+        this.directions = [
+           [this.x - 1, this.y - 1],
+           [this.x, this.y - 1],
+           [this.x + 1, this.y - 1],
+           [this.x - 1, this.y],
+           [this.x + 1, this.y],
+           [this.x - 1, this.y + 1],
+           [this.x, this.y + 1],
+           [this.x + 1, this.y + 1]
+       ];
+ 
+    }
+    chooseCell(ch) {
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length){
+                if (matrix[y][x] == ch) {
+                    found.push(this.directions[i]);
+                }
+            }   
+        }
+        return found;
+    }
+}
+
 class Grass {
     constructor(x, y, index) {
         this.x = x;
@@ -45,6 +78,21 @@ class Grass {
         }
     }
 }
+
+class Grass extends LivingCreature {
+
+    mul() {
+        this.multiply++;
+        var newCell = random(this.yntrelVandak(0));
+        if(this.multiply >= 8 && newCell) {
+            var newGrass = new Grass(newCell[0],newCell[1], this.index);
+            grassArr.push(newGrass);
+            matrix[newCell[1]][newCell[0]] = this.index;
+            this.multiply = 0;
+        }
+    }
+}
+
 
 
 class GrassEater {
@@ -156,6 +204,31 @@ class GrassEater {
         }
     }
 }
+
+class GrassEater extends LivingCreature {
+    constructor(x, y, index){
+        super(x, y, index);
+        this.energy = 8;
+    }
+   getNewCoordinates() {
+       this.directions = [
+           [this.x - 1, this.y - 1],
+           [this.x, this.y - 1],
+           [this.x + 1, this.y - 1],
+           [this.x - 1, this.y],
+           [this.x + 1, this.y],
+           [this.x - 1, this.y + 1],
+           [this.x, this.y + 1],
+           [this.x + 1, this.y + 1]
+       ];
+   }
+   chooseCell(character) {
+       this.getNewCoordinates();
+       return super.chooseCell(character);
+   }
+   // eat, mul, move, die
+}
+
 
 class Predator {
     constructor(x, y, index) {
